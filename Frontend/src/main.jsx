ed447bc8be287e8f8017c1d2f131592767440b45
Axel Moncada Desktop <axelemoncada@gmail.com>
2025-02-18 12:57:08 -0500

    Creacion del README

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Menu from './menu.jsx'
 import Slider from './slider.jsx'
 import Products from './products.jsx'
@@ -12,7 +11,6 @@ createRoot(document.getElementById('root')).render(
     <Menu />
     <Slider />
     <Products />
-    <App />
     <Footer />
   </StrictMode>,
 )
